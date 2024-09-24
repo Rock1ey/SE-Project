e010f6cfb35f6637868f5f82f444e56a95f036e8
Rock1ey <2061218098@qq.com>
2024-09-24 17:01:39 +0800

    some code test(forecast)
    Signed-off-by: Rock1ey <2061218098@qq.com>

--- SE-Project/SE-Project/ContentView.swift
+++ SE-Project/SE-Project/ContentView.swift
@@ -9,16 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            
+            ForecastView(day: "Mon", isRainy: true, high: 70, low: 50)
+            
+            ForecastView(day: "Tue", isRainy: false, high: 60, low: 40)
+            
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct ForecastView: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy{
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy{
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+        
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
+}
+
