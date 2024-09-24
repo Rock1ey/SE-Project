4df2cbed307eb6bb8edc2528890822166ebb3f45
Rock1ey <2061218098@qq.com>
2024-09-24 16:52:29 +0800

    create project and commit test
    Signed-off-by: Rock1ey <2061218098@qq.com>

---
+++ SE-Project/SE-Project/SE_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SE_ProjectApp.swift
+//  SE-Project
+//
+//  Created by admin on 2024/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SE_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
